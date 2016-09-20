196e36ce6073b8f0302d5347f322746d75789a10
honeybunhero <crv1124@hotmail.com>
2016-09-20 16:39:54 -0400

    Add files via upload

---
+++ GD205 Week 4/Assets/Script/Dancing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dancing : MonoBehaviour {
+	bool dance;
+	public enum groupRotate {group1, group2}
+	public groupRotate Group = groupRotate.group1;
+
+	Quaternion rotRight, rotLeft;
+
+	void Start () 
+	{
+
+		dance = false;
+		rotLeft = Quaternion.Euler (0, -1, 0);
+		rotRight = Quaternion.Euler (0, 1, 0);
+	}
+
+	void Update()
+	{
+		if(Group == groupRotate.group1){
+			transform.rotation *= rotRight;
+		}
+		if(Group == groupRotate.group2){
+			transform.rotation *= rotRight;
+		}
+	}
+}
+	
---
+++ GD205 Week 4/Assets/Script/PlayerMovment.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovment : MonoBehaviour {
+
+	public float WalkSpeed;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xWalk = Input.GetAxis ("Horizontal");
+		float zWalk = Input.GetAxis ("Vertical");
+		transform.Translate (xWalk * WalkSpeed,0,zWalk * WalkSpeed);
+	}
+}
